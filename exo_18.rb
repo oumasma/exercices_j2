d9b10cc2f34b2d20ad615fa548fff66f10d48bf4
oumasma <oumasma93@hotmail.fr>
2020-07-08 00:55:53 +0400

    ex18

--- exo_18.rb
+++ exo_18.rb
@@ -1,14 +1,6 @@
-puts "Donne moi ton age"
-age = Integer(gets)
-year = 0
-
-while year <= age
-	puts "Il y a #{age-year} ans, tu avais #{year} ans"
-	if (year == age/2)
-			puts "Il y a #{year}, tu avais la moitié de l'age que tu as aujourd'hui"
-	end
-	year = year+1
-end 
+utilisateurs = ["jean.dupont.01@email.fr", 
 
 
 
+]
+puts utilisateurs

6b3119937331c1b798cc74891be2468877b8f9f6
oumasma <oumasma93@hotmail.fr>
2020-07-08 01:04:18 +0400

    ex19

--- exo_19.rb
+++ exo_19.rb
@@ -1,4 +1,6 @@
-# Prends le programme exo_18.rb et créé un programme exo_19.rb qui va reprendre l'array des emails créés, et n'afficher que les emails avec un nombre pair.
+utilisateurs = ["jean.dupont.01@email.fr", 
 
 
 
+]
+puts utilisateurs

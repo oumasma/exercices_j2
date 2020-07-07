90a4843b2be5a2ec0dd73cebfe6c0a44ff689111
oumasma <oumasma93@hotmail.fr>
2020-07-07 21:32:39 +0400

    modi exo 12

--- exo_12.rb
+++ exo_12.rb
@@ -1,7 +1,17 @@
 puts "Donne-moi un nombre s'il te plait ;)"
 number = gets.chomp
+compteur = 0
 
-number 
+while compteur < number.to_i
+	puts "" + compteur.to_s
+	compteur += 1
+end
+	puts number
 
 
-# idem ex 10 avec.to_i
+
+ 
+
+
+# Écris un programme exo_12.rb qui demande un nombre à l'utilisateur, 
+#puis qui compte jusqu'à ce nombre.
